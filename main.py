cc652c51f12d8532a66d528f406eb071a000062f
JFong5 <97566318+JFong5@users.noreply.github.com>
2022-11-21 22:59:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pymongo
 from pymongo import MongoClient
 import json
 
-
 def inputJsonName():
     '''
     Prompts the user for a Json File name
@@ -10,7 +9,6 @@ def inputJsonName():
     '''
     jsonName = input("Input the json file name you would like to insert. \n")
     return jsonName
-
 
 def inputPortNum():
     '''
@@ -20,33 +18,37 @@ def inputPortNum():
     portNum = int(input("Please input a port number. \n"))
     return portNum
 
-
 def main():
-    # Prompt user for portNum and insert it to client name
+    #Prompt user for portNum and insert it into client name
     portNum = inputPortNum()
     client = MongoClient('mongodb://localhost:' + str(portNum))
-    dbs = client.list_database_names()
 
-    # Checks if the database exists and drops
+    #Checks if the database name exists
+    dbs = client.list_database_names()
     if '291db' in dbs:
-        # if exists removes database
+        # if exists removes the database
         print("Database already exists.")
         db = client["291db"]
         db.data.drop()
         print("Updating data base....")
 
-    # Checks
+    #Connects database 291db to client and create collection
     db = client["291db"]
     collection = db["dplb"]
 
+    #Prompt user for json fileName and process items one row at a time
+    #inserts items into collection_list
+    collection_list = []
     fileName = inputJsonName()
     with open(fileName) as file:
-        data = json.loads(file)
-
+        for item in file:
+            dic = json.loads(item)
+            collection_list.append(dic)
+    
+    #insert collection list into database
     collection.delete_many({})
-    collection.insert_many(data)
-
+    collection.insert_many(collection_list)
     print("Collection Created!")
 
-
 main()
+
